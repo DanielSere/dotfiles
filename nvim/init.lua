8917fd9792ac80d132975d1661281a3f6207ecdf
Daniel Sere <87248193+DanielSere@users.noreply.github.com>
2023-01-12 13:58:33 -0600

    Nothing

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,16 @@
-require 'user.plugins'
 require 'user.options'
+require 'user.lsp'
+require 'user.cmp'
+require 'user.treesitter'
+require 'user.plugins'
 require 'user.keymaps'
 require 'user.autocommands'
-require 'user.lualine'
 require 'user.comments'
 require 'user.indentline'
 require 'user.gitsigns'
 require 'user.which-key'
 require 'user.telescope'
-require 'user.treesitter'
-require 'user.lsp'
-require 'user.cmp'
-
+require 'user.colorizer'
+require 'user.lualine'
 --[[ Modeline ]]
 -- vim: ts=4 sw=4 sts=4 noet
