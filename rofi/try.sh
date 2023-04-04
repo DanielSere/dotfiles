488c6a9c8631794f77efcbc20cca902d24a34674
Daniel Sere <87248193+DanielSere@users.noreply.github.com>
2023-04-04 12:08:13 -0600

    changes

---
+++ rofi/try.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+((nmcli --fields "SECURITY,SSID" device wifi list | sed 1d | sed 's/  */ /g' | sed -E "s/WPA*.?\S/ /g" | sed "s/^--/ /g" | sed "s/  //g" | sed "/--/d")& rofi -dmenu -p 'Getting list of available Wi-Fi networks...' )
